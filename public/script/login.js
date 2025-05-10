f276b62bf46cd585bdc5cf3c51f2212fe32ffdd7
hiepvanle <hiep.le2102116@std.ttu.edu.vn>
2025-05-10 13:49:58 +0700

    update ReactJS

--- public/script/login.js
+++ public/script/login.js
@@ -27,9 +27,10 @@ const Login = () => {
             <form onSubmit={handleLogin}>
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-                <button type="submit">Login</button>
+                <button type="submit" value="Login" onClick={() => window.location.href = "/home.html"}>Login</button>
+                <button type="button" onClick={() => window.location.href = "/register.html"}>Register</button>
             </form>
-            <Link to="/register">Register</Link>
+            <a onClick={() => window.location.href = "/register.html"}>Register</a>
         </div>
     );
 };
